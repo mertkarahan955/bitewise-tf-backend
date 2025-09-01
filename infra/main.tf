dacd8cb6dce75c718fab04d9b484826da8fb38cc
mert karahan <mkarahan@eseye.com>
2025-09-01 04:55:26 +0300

    permission api

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,16 @@
-# Gerekli API'ler
 resource "google_project_service" "services" {
   for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
     "run.googleapis.com",
     "artifactregistry.googleapis.com",
     "cloudbuild.googleapis.com"
   ])
+  project            = var.project_id
   service            = each.value
   disable_on_destroy = false
 }
+
 
 # Artifact Registry (Docker)
 resource "google_artifact_registry_repository" "repo" {
